21ed3508486370e28084e32a36a71ee69f74f199
sdxsd <willmaguire@icloud.com>
2023-09-07 15:38:23 +0200

    The script works!!!!!!!!!

--- src/SRT-to-txt.py
+++ src/SRT-to-txt.py
@@ -4,10 +4,22 @@ import srt
 import sys
 
 print("Formatting: " + sys.argv[1])
-
 srtFile = open(sys.argv[1])
 subtitlesText = srtFile.read()
-subtitlesGenerator = srt.parse(srtFile)
+subtitlesList = list(srt.parse(subtitlesText))
 outputFileText = open("../output/separatedText.txt", "w")
-for sub in srt.parse(subtitlesText):
-    outputFileText.write(sub.content + "\n")
+for i in subtitlesList:
+    outputFileText.write(i.content + "\n")
+print("Formatted: " + sys.argv[1])
+input("Press enter when processed data is placed into output/newContent.txt...")
+newContentFile = open("../input/newContent.txt", "r")
+newContentLines = newContentFile.readlines()
+i = 0
+for sub in subtitlesList:
+    if (i > len(newContentLines) - 1):
+        sub.content = sub.content
+    else:
+        sub.content = newContentLines[i]
+    i += 1
+finalOutputFile = open("../output/processedOutput.srt", "w")
+finalOutputFile.write(srt.compose(subtitlesList))
